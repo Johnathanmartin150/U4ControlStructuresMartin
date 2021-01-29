e0d826d36337729cb90f9e8ffa7d8de0b604af9b
Johnathanmartin150 <martijoh001@warren.k12.in.us>
2021-01-29 12:32:53 +0000

    updated notes

--- L3-For Loops/src/Main.java
+++ L3-For Loops/src/Main.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
-public class Main{
+public class Main {
     public static void main(String[] args) {
+      /*  String word = "Programming";
 
-        for(int number = 1; number < 10; number++){
-            System.out.println(number);
+        for(int i = 2; i < 10; i++){
+            System.out.println(i*2);
         }
+
+        System.out.println();*/
+  /* for(int 1 = 10; 1 > 0; 1--){
+       System.out.println(1);
+        }*/
+   for(int i =1; i < 30; i+=3){
+       
+       }
     }
 }
